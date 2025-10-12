fa0f657c8aa0f8f33a27eb33ab9aa64510c3495c
srahavi <srahavi@huronconsultinggroup.com>
2025-10-11 17:34:01 -0700

    Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
-import { ThemeProvider, createTheme, CssBaseline, Container } from '@mui/material';
+import { ThemeProvider, createTheme, CssBaseline, Container, AppBar, Toolbar, Tabs, Tab } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Questionnaire from './Components/Questionnaire';
+import Report from './Components/Report';
 
-const theme = createTheme(); // You can customize this if needed
+const theme = createTheme();
+
+const NavTabs: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    navigate(newValue);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Tabs value={location.pathname} onChange={handleChange} textColor="inherit" indicatorColor="secondary">
+          <Tab label="Home" value="/" />
+          <Tab label="Report" value="/report" />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container maxWidth="sm" sx={{ mt: 4 }}>
-        <Questionnaire />
-      </Container>
+      <Router>
+        <NavTabs />
+        <Container maxWidth="sm" sx={{ mt: 4 }}>
+          <Routes>
+            <Route path="/" element={<Questionnaire />} />
+            <Route path="/report" element={<Report />} />
+          </Routes>
+        </Container>
+      </Router>
     </ThemeProvider>
   );
 };
---
+++ src/Components/Report.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Report: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Report Page
+      </Typography>
+      <Typography>
+        This is where your report content will go.
+      </Typography>
+    </div>
+  );
+};
+
+export default Report;
