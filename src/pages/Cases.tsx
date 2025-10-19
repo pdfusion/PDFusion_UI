411af2a8baa14acf68555bc92e3a7f7c8323c638
srahavi <srahavi@huronconsultinggroup.com>
2025-10-19 14:35:17 -0700

    Fix Cases

--- src/pages/Cases.tsx
+++ src/pages/Cases.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import { useCasesData } from '../contexts/CasesDataContext';
+import React, { useEffect, useState } from 'react';
+import { type CaseDataType, defaultCaseData } from '../contexts/CasesDataContext';
 import { useCasesDataAPI } from '../hooks/useCasesDataAPI';
 
 const Cases: React.FC = () => {
-  const { casesData } = useCasesData();
+  const [casesData, setCasesData] = useState<CaseDataType[]>([defaultCaseData]);
   const { getCases } = useCasesDataAPI();
 
   useEffect(() => {
     const logCases = async () => {
-      const cases = await getCases();
-      console.log('📦All cases from localStorage:', cases);
+      const casesDataRes = await getCases();
+      setCasesData(casesDataRes);
+      console.log('sr log casesData:', casesDataRes);
     };
 
     logCases();
-  }, [casesData]); // re-run whenever casesData changes
+  }, []);
 
   return (
     <div>
